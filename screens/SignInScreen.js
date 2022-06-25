2b555fb8e9971d2d155aa80a299d7aba5557a21d
Haris Samingan <muhamad_haris@hotmail.com>
2022-06-25 19:32:25 +0800

    Add authentication

--- screens/SignInScreen.js
+++ screens/SignInScreen.js
@@ -10,46 +10,64 @@ import {
   Keyboard,
 } from "react-native";
 import AsyncStorage from "@react-native-async-storage/async-storage";
+import axios from "axios";
+
+const API = "https://testing.haris-samingan.repl.co";
+const API_LOGIN = "/auth";
 
 export default function SignInScreen({ navigation }) {
   const [username, setUsername] = useState("");
   const [password, setPassword] = useState("");
   const [errorText, setErrorText] = useState("");
 
-  function login() {
+  async function login() {
+    console.log("---- Login time ----");
     Keyboard.dismiss();
-    AsyncStorage.setItem("token", "demo_token");
-    navigation.navigate("Account");
+
+    try {
+      const response = await axios.post(API + API_LOGIN, {
+        username,
+        password,
+      });
+      console.log("Success logging in!");
+      console.log(response);
+
+      AsyncStorage.setItem("token", response.data.access_token);
+      navigation.navigate("Account");
+    } catch (error) {
+      console.log("Error logging in!");
+      console.log(error.response);
+
+      setErrorText(error.response.data.description);
+    }
   }
 
   return (
-    <TouchableWithoutFeedback onPress={Keyboard.dismiss}>
-      <View style={styles.container}>
-        <Text style={styles.title}>Sign in to blog</Text>
-        <Text style={styles.fieldTitle}>Username</Text>
-        <TextInput
-          style={styles.input}
-          autoCapitalize="none"
-          autoCorrect={false}
-          value={username}
-          onChangeText={(input) => setUsername(input)}
-        />
-        <Text style={styles.fieldTitle}>Password</Text>
-        <TextInput
-          style={styles.input}
-          autoCapitalize="none"
-          autoCompleteType="password"
-          autoCorrect={false}
-          secureTextEntry={true}
-          value={password}
-          onChangeText={(input) => setPassword(input)}
-        />
-        <TouchableOpacity onPress={login} style={styles.loginButton}>
-          <Text style={styles.buttonText}>Log in</Text>
-        </TouchableOpacity>
-        <Text style={styles.errorText}>{errorText}</Text>
-      </View>
-    </TouchableWithoutFeedback>
+    <View style={styles.container}>
+      <Text style={styles.title}>Sign in to blog</Text>
+      <Text style={styles.fieldTitle}>Username</Text>
+      <TextInput
+        style={styles.input}
+        autoCapitalize="none"
+        autoCorrect={false}
+        value={username}
+        onChangeText={(input) => setUsername(input)}
+      />
+      <Text style={styles.fieldTitle}>Password</Text>
+      <TextInput
+        style={styles.input}
+        autoCapitalize="none"
+        autoCompleteType="password"
+        autoCorrect={false}
+        secureTextEntry={true}
+        value={password}
+        onChangeText={(input) => setPassword(input)}
+      />
+      <TouchableOpacity onPress={login} style={styles.loginButton}>
+        <Text style={styles.buttonText}>Log in</Text>
+      </TouchableOpacity>
+      <Text style={styles.errorText}>{errorText}</Text>
+    </View>
   );
 }
 
